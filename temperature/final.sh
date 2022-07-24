1de6f06e5c256aa9330b494f8c73c5cde6ec0f4b
quanta1985 <109708171+quanta1985@users.noreply.github.com>
2022-07-24 11:50:44 +0700

    Update final.sh

--- temperature/final.sh
+++ temperature/final.sh
@@ -5,15 +5,15 @@ yearend=iyend
 
 var='tas tasmax tasmin'
 for ivar in $var;do #for each variable
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.201* tmp.merge.21.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.202* tmp.merge.22.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.203* tmp.merge.23.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.204* tmp.merge.24.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.205* tmp.merge.25.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.206* tmp.merge.26.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.207* tmp.merge.27.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.208* tmp.merge.28.nc
-  cdo -O -mergetime tmp.bcsd.${ivar}fix.209* tmp.merge.29.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.201* tmp.merge.21.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.202* tmp.merge.22.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.203* tmp.merge.23.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.204* tmp.merge.24.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.205* tmp.merge.25.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.206* tmp.merge.26.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.207* tmp.merge.27.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.208* tmp.merge.28.nc
+  cdo -O -mergetime tmp.bcsd.${ivar}.209* tmp.merge.29.nc
   cdo -O -mergetime tmp.merge* bcsd_${ivar}_${model}_ssp585_${yearbgn}-${yearend}_daily.nc # Revised version with trend bias removal
 done 
 
